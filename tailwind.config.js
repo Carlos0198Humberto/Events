c8f354a04a4c68af6fb53230690dc64a421ca757
Humb3rSec <98humbertocarlos@gmail.com>
2026-03-30 12:51:50 -0600

    fix: rename tailwind config to .ts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,13 @@
-import type { Config } from 'tailwindcss'
-
-const config: Config = {
+/** @type {import('tailwindcss').Config} */
+const config = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-export default config
+};
+export default config;
